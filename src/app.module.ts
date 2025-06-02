71c1ccdd9f8c37da18bb4c3b44ca3f4613d3bbb4
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-06-03 00:40:12 +0530

    feat: add comments and users modules with controllers, services, and DTOs; configure MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env.prod'],
+      expandVariables: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+        
+        if (!uri) {
+          throw new Error('MONGODB_URI environment variable is not defined');
+        }
+
+        return {
+          uri,
+          retryWrites: true,
+          w: 'majority',
+          maxPoolSize: 10,
+          serverSelectionTimeoutMS: 5000,
+          socketTimeoutMS: 45000,
+          connectTimeoutMS: 10000,
+          heartbeatFrequencyMS: 30000,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
